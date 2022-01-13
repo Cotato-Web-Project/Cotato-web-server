e48301c9c49bf845af3fc8eff37d078cbc8564a1
dlrjs2360 <lhg3670@naver.com>
2022-01-13 21:14:30 +0900

    add routing

--- index.js
+++ index.js
@@ -2,19 +2,35 @@ const express = require("express")
 const app = express()
 const port = 3000
 const bodyParser = require("body-parser")
-
+const router = require("express").Router()
 const config = require("./config/key")
+const boardRouter = require("./routes/board")
+const updateRouter = require("./routes/update")
+const writeRouter = require("./routes/write")
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.use("/board", boardRouter)
+app.use("/update", updateRouter)
+app.use("/write", writeRouter)
+
 const mongoose = require("mongoose")
-const { json } = require("body-parser")
+const req = require("express/lib/request")
+// const { json } = require("body-parser")
+
 mongoose
   .connect(config.mongoURI, {})
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err))
 
-app.get("/", (req, res) => res.send("우와!"))
+app.use((req, res, next) => {
+  res.sendStatus(404)
+})
+
+app.use((error, req, res, next) => {
+  console.error(error)
+  res.sendStatus(500)
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ routes/write.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const router = express.Router()
+
+router.get("/", (req, res, next) => {
+  res.send("글작성페이지")
+})
+
+module.exports = router
