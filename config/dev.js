c33ed457fac96f29120c61842c20b244deef104d
dlrjs2360 <lhg3670@naver.com>
2022-01-13 22:37:48 +0900

    dev 추가

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://dlrjs4867:ab3670@cotato-web.nfid2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+}
--- routes/board.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-router.get("/", (req, res, next) => {
-  res.send("게시판 홈페이지")
-})
-
-module.exports = router
