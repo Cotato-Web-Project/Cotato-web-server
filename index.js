796163d7ef8229711ba7b5a07776a32b5ec3af40
dlrjs2360 <lhg3670@naver.com>
2022-01-15 13:24:03 +0900

    #10

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const express = require("express")
 const app = express()
 const port = 3000
 const bodyParser = require("body-parser")
-const router = require("express").Router()
 const config = require("./config/key")
-const boardRouter = require("./routes/boardhome")
-const updateRouter = require("./routes/postupdate")
-const writeRouter = require("./routes/postwrite")
-const onboardRouter = require("./routes/onBoard")
+const posts = require("./models/post")
+// const boardRouter = require("./routes/boardhome")
+// const updateRouter = require("./routes/postupdate")
+// const writeRouter = require("./routes/postwrite")
+// const onboardRouter = require("./routes/onBoard")
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.use("/board", boardRouter)
-app.use("/board/:id", onboardRouter)
-app.use("/update", updateRouter)
-app.use("/write", writeRouter)
+// app.use("/board", boardRouter)
+// app.use("/board/:id", onboardRouter)
+// app.use("/update", updateRouter)
+// app.use("/write", writeRouter)
 
 const mongoose = require("mongoose")
 const req = require("express/lib/request")
@@ -32,3 +32,78 @@ mongoose
   .catch((err) => console.log(err))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+//게시글 모두 가져오기 API (getAllpost)
+app.get("/board", function (req, res) {
+  posts.find({}, function (err, posts) {
+    if (err) return res.json(err)
+    res.send(posts)
+  })
+})
+
+//게시글 등록 API(createPost)
+app.post("/board/create", (req, res, next) => {
+  const { title, content, postid } = req.body.data.post
+  console.log(req.body)
+
+  const postModel = new posts()
+  postModel.title = title
+  postModel.content = content
+  postModel.postid = postid
+
+  postModel
+    .save()
+    .then((newPost) => {
+      console.log("Create 완료")
+      res.status(200).json({
+        message: "Create success",
+        data: {
+          post: newPost,
+        },
+      })
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: err,
+      })
+    })
+})
+
+//게시판 검색 기능
+app.get("/board/search", async (req, res) => {
+  let options = []
+  if (req.query.option == "title") {
+    options = [{ title: new RegExp(req.query.content) }]
+  } else if (req.query.option == "content") {
+    options = [{ content: new RegExp(req.query.content) }]
+  } else if (req.query.option == "title+content") {
+    options = [
+      { title: new RegExp(req.query.content) },
+      { content: new RegExp(req.query.content) },
+    ]
+  } else {
+    const err = new Error("검색 옵션이 없습니다.")
+    err.status = 400
+    throw err
+  }
+  console.log(options)
+  posts.find({ $or: options }, (err, result) => {
+    res.json(result)
+  })
+})
+
+// 선택된 게시글 정보를 불러오는 요청 API(toPost)
+app.get("/board/:id", (req, res) => {
+  posts.findOne({ postid: parseInt(req.params.id) }, (err, post) => {
+    if (err) return res.json(err)
+    res.send(post)
+  })
+})
+
+//게시글 삭제 API(deletePost)
+app.delete("/board/:id", (req, res) => {
+  posts.deleteOne({ postid: parseInt(req.params.id) }, (err, post) => {
+    if (err) return res.json(err)
+    res.redirect("/board")
+  })
+})
--- routes/onBoard.js
+++ routes/onBoard.js
@@ -1,46 +1,46 @@
-const express = require("express")
-const router = express.Router()
-const posts = require("../models/post")
+// const express = require("express")
+// const router = express.Router()
+// const posts = require("../models/post")
 
-//선택된 게시글을 보여주는 페이지
-router.get("/", (req, res, next) => {
-  res.send("onBoard 홈페이지")
-})
+// //선택된 게시글을 보여주는 페이지
+// router.get("/", (req, res, next) => {
+//   res.send("onBoard 홈페이지")
+// })
 
-//게시글 등록 API(createPost) -> control폴더 만들어서 따로 보관해야함.
-router.post("/create", (req, res, next) => {
-  const { title, content, postid } = req.body.data.post
-  console.log(req.body)
+// //게시글 등록 API(createPost) -> control폴더 만들어서 따로 보관해야함.
+// router.post("/create", (req, res, next) => {
+//   const { title, content, postid } = req.body.data.post
+//   console.log(req.body)
 
-  const postModel = new posts()
-  postModel.title = title
-  postModel.content = content
-  postModel.postid = postid
+//   const postModel = new posts()
+//   postModel.title = title
+//   postModel.content = content
+//   postModel.postid = postid
 
-  postModel
-    .save()
-    .then((newPost) => {
-      console.log("Create 완료")
-      res.status(200).json({
-        message: "Create success",
-        data: {
-          post: newPost,
-        },
-      })
-    })
-    .catch((err) => {
-      res.status(500).json({
-        message: err,
-      })
-    })
-})
+//   postModel
+//     .save()
+//     .then((newPost) => {
+//       console.log("Create 완료")
+//       res.status(200).json({
+//         message: "Create success",
+//         data: {
+//           post: newPost,
+//         },
+//       })
+//     })
+//     .catch((err) => {
+//       res.status(500).json({
+//         message: err,
+//       })
+//     })
+// })
 
-//선택된 게시글 정보 요청 API(toPost) -> control폴더 만들어서 따로 보관해야함.
-router.get("/", (req, res) => {
-  posts.findOne({ postid: parseInt(req.params.id) }, (err, post) => {
-    if (err) return res.json(err)
-    res.send(post)
-  })
-})
+// //선택된 게시글 정보 요청 API(toPost) -> control폴더 만들어서 따로 보관해야함.
+// router.get("/", (req, res) => {
+//   posts.findOne({ postid: parseInt(req.params.id) }, (err, post) => {
+//     if (err) return res.json(err)
+//     res.send(post)
+//   })
+// })
 
-module.exports = router
+// module.exports = router
